944ca9ad2b2445dd7b42bf9f70f263aeeec33748
Santiago Comeglio <santicome05@gmail.com>
2025-02-19 22:26:34 -0300

    Mostrar Datos Vuelo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ComponentVuelo } from "./components/UI/ComponentVuelo/ComponentVuelo";
 import styles from "./App.module.css";
 import { Header } from "./components/UI/Header/Header";
+import { ShowVuelo } from "./components/UI/ShowVuelo/ShowVuelo";
 
 function App() {
   return (
     <div className={styles.containerMain}>
       <Header />
       <ComponentVuelo />
+      <ShowVuelo />
     </div>
   );
 }
---
+++ src/components/UI/ShowVuelo/ShowVuelo.tsx
@@ -0,0 +1,36 @@
+import styles from "./ShowVuelo.module.css";
+
+export const ShowVuelo = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Vuelo</h2>
+      <p className={styles.line}></p>
+      <div className={styles.showDestiny}>
+        <div className={styles.origen}>
+          <h3>Origen</h3>
+          <p>Mendoza, Argentina</p>
+        </div>
+
+        <div className={styles.destino}>
+          <h3>Destino</h3>
+
+          <p>Buenos, Argentina</p>
+        </div>
+      </div>
+      <div className={styles.showDays}>
+        <div className={styles.ida}>
+          <p>Domingo</p>
+          <p>18 Diciembre</p>
+        </div>
+        <div className={styles.regreso}>
+          <p>Sabado</p>
+          <p>30 Septiembre</p>
+        </div>
+      </div>
+
+      <div className={styles.showClass}>
+        <p>2 pasajeros, Bussines</p>
+      </div>
+    </div>
+  );
+};
