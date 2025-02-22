979e7a46c16e73cd0e4e5106e71e5ce97b287955
Santiago Comeglio <santicome05@gmail.com>
2025-02-21 21:12:43 -0300

    Haciendo retoques

--- src/components/UI/DetailBuy/DetailBuy.tsx
+++ src/components/UI/DetailBuy/DetailBuy.tsx
@@ -5,28 +5,51 @@ export const DetailBuy = () => {
     <div className={styles.container}>
       <div className={styles.showDataIda}>
         <div className={styles.showDataDD}>
-          <p className={styles.txtDestino}>Mendoza - Buenos Aires</p>
+          <p className={styles.txtDestino}><b>Mendoza - Buenos Aires</b></p>
           <p className={styles.txtDia}>Jueves, 18 de enero</p>
         </div>
 
         <div className={styles.showDataAD}>
-
-            <div className={styles.containerData}>
-
-          <p className={styles.txt}>MDZ</p>
-          <span className={styles.line}></span>
-          <p className={styles.txt}>AEP</p>
+          <div className={styles.containerData}>
+            <p className={styles.txt}><b>MDZ</b></p>
+            <div className={styles.lineContainer}>
+              <span className={styles.directo}>Directo</span>
+              <span className={styles.line}></span>
             </div>
+            <p className={styles.txt}><b>AEP</b></p>
+          </div>
 
           <div className={styles.showDuracion}>
             <p className={styles.txtDuration}>Duración</p>
-            <p className={styles.txtTime}>1h 40min</p>
+            <p className={styles.txtTime}>1h 40m</p>
           </div>
         </div>
       </div>
-      
 
-      <div className={styles.showDataVuelta}></div>
+      <span className={styles.lineDiv}></span>
+
+      <div className={styles.showDataVuelta}>
+      <div className={styles.showDataDD}>
+          <p className={styles.txtDestino}><b>Buenos Aires - Mendoza</b></p>
+          <p className={styles.txtDia}>Viernes, 26 de enero</p>
+        </div>
+
+        <div className={styles.showDataAD}>
+          <div className={styles.containerData}>
+            <p className={styles.txt}><b>AEP</b></p>
+            <div className={styles.lineContainer}>
+              <span className={styles.directo}>Directo</span>
+              <span className={styles.line}></span>
+            </div>
+            <p className={styles.txt}><b>MDZ</b></p>
+          </div>
+
+          <div className={styles.showDuracion}>
+            <p className={styles.txtDuration}>Duración</p>
+            <p className={styles.txtTime}>1h 40m</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
