7eab58e5f3c49c4873f8db8a7f8bcd0a41b76062
Santiago Comeglio <santicome05@gmail.com>
2025-02-27 20:18:02 -0300

    ModificandoCarpetas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import HeroSection from "./components/UI/HeroSection/HeroSection";
 import './index.css'
 import NavBar from "./components/UI/NavBar/NavBar";
-import { ShowVuelo } from "./components/ShowVuelo/ShowVuelo";
-import { ComponentVuelo } from "./components/ComponentVuelo/ComponentVuelo";
+import { ShowVuelo } from "./components/UI/ShowVuelo/ShowVuelo";
+import { ComponentVuelo } from "./components/UI/ComponentVuelo/ComponentVuelo";
 
 
 function App() {
